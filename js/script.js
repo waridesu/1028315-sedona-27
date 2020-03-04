59f67621dd0a455243e3a529df03b72409ccdb8e
Denis <waridesu@gmail.com>
2020-03-04 17:13:26 +0200

    скрипт модалки

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ var togBtn = document.querySelector(".search");
 var modal = document.querySelector(".modal-container");
 
 togBtn.addEventListener("click", function() {
-  modal.classList.add("modal-hide");
+  modal.classList.toggle("modal-show");
 });
