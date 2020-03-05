bb98a54731d2149a4aa681e385b7ea7024a7d862
Denis <waridesu@gmail.com>
2020-03-05 23:49:15 +0200

    И еще раз оформление, скрипт, ие 11 правки

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ var togBtn = document.querySelector(".search");
 var modal = document.querySelector(".modal-container");
 
 togBtn.addEventListener("click", function() {
-  modal.classList.toggle("modal-show");
+  modal.classList.toggle("modal-hide");
 });
