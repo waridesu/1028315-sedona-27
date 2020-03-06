9ca7cc115c552e2b357382af156f4189e3479b96
Denis <waridesu@gmail.com>
2020-03-06 23:49:16 +0200

    Возможно верно

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var togBtn = document.querySelector(".search");
 var modal = document.querySelector(".modal-container");
-
+modal.classList.add("modal-hide");
 togBtn.addEventListener("click", function() {
   modal.classList.toggle("modal-hide");
 });
